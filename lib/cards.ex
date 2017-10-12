5ec8640155165bc5841a957838cbb9fb1a0266c4
Matheus Mina <mfbmina@gmail.com>
2017-10-12 00:51:02 -0300

    Add documentation to the project

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,4 +1,11 @@
 defmodule Cards do
+  @moduledoc """
+    Provides methods to handling decks of cards
+  """
+
+  @doc """
+    Returns a list of cards
+  """
   def create_deck do
     values = ["Ace", "Two", "Three"]
     suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
@@ -8,23 +15,58 @@ defmodule Cards do
     end
   end
 
+  @doc """
+    Shuffles a list of cards
+  """
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Check if a `card` exists in a list of cards
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "King of Spades")
+      false
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Divides a deck into a hand and the remainer of the deck.
+    The `hand_size` indicates how many cards should be in the hand.
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
 
+  @doc """
+    Saves the deck into a file.
+    The `filename` indicates the name of the file.
+  """
   def save(deck, filename) do
     binary = :erlang.term_to_binary(deck)
     File.write(binary, filename)
   end
 
+  @doc """
+    Retrives a deck from a file.
+    The `filename` indicates the name of the file.
+  """
   def load(filename) do
     { status, binary } = File.read(filename)
     case status do
@@ -33,6 +75,10 @@ defmodule Cards do
     end
   end
 
+  @doc """
+    Creates a playing hand by creating a deck, shuffleing it and spliting it into a hand.
+    The `hand_size` indicates how many cards should be in the hand.
+  """
   def create_hand(hand_size) do
     Cards.create_deck
     |> Cards.shuffle
